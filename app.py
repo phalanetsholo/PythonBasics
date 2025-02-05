5006cfbe6538e9aabb84fdfe73123d42405d0204
phalanetsholo <phalanetsholo@gmail.com>
2025-02-05 14:45:08 +0200

    added  boolean and float

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 print('Tsholo') #my first print push
 
 #variables
+#STRINGS
+
 name = "Tsholo"
 print(name)
+
 
 
 first_name = input("What is your name?  ")
@@ -15,6 +18,31 @@ print("Are you " + first_age + "?")
 #INTERGER 
 
 age = 21
+players = 2
+quantity = 5
 
-print("You are " + str(age) + " years old")
-print("You are ", age, " years old")
+
+print(f"You are {age} years old")
+print(f"You are {players} players online")
+print(f"You would like to buy {quantity} items")
+
+#FLOAT
+
+gpa = 3.2
+distance = 2.5
+price = 10.99
+
+print(f"Your {gpa}")
+print(f"You ran {distance} Km")
+print(f"The price is ${price}")
+
+#BOOLEAN
+
+online = True
+for_sale = False
+running = True
+
+
+print(f"Are you online? {online}")
+print(f"Is the item for sale?: {for_sale}")
+print(f"Game running{running}")
