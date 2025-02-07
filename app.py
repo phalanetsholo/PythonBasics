a9075e29e03bca6dae7804fb129bde65f03e43fe
phalanetsholo <phalanetsholo@gmail.com>
2025-02-07 08:53:35 +0200

    added more exapmles

--- app.py
+++ app.py
@@ -17,6 +17,12 @@ print("Are you " + first_age + "?")
 
 #INTERGER 
 
+dragons = 3
+dead_dragons = 2
+
+total_dragons = dragons - dead_dragons
+print(total_dragons)
+
 age = 21
 players = 2
 quantity = 5
@@ -27,6 +33,13 @@ print(f"You are {players} players online")
 print(f"You would like to buy {quantity} items")
 
 #FLOAT
+
+covid_bc = 1203
+population_bc = 5071000
+prob = covid_bc / population_bc
+print(prob)
+
+print(type(prob))
 
 gpa = 3.2
 distance = 2.5
@@ -46,3 +59,5 @@ running = True
 print(f"Are you online? {online}")
 print(f"Is the item for sale?: {for_sale}")
 print(f"Game running{running}")
+
+
